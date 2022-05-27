a20017eaef38619f8c1d7d2ffcd9b1b6a04138a2
SeunghoChoi1 <99311667+SeunghoChoi1@users.noreply.github.com>
2022-05-27 14:19:29 +0900

    LoginDTO csh

--- hotel/src/main/java/com/care/hotel/login/DTO/LoginDTO.java
+++ hotel/src/main/java/com/care/hotel/login/DTO/LoginDTO.java
@@ -2,4 +2,24 @@ package com.care.hotel.login.DTO;
 
 public class LoginDTO {
 
+	private String memberId;
+	private String pw;
+	
+	
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	
+	
+	
+	
 }
