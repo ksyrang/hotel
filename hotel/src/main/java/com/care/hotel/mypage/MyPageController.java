f41c56b1bb271cd90a9f8db8253dfdeb6eec8b92
YoongheeLee <whiteyhl@hanmail.net>
2022-05-31 16:43:36 +0900

    LYH 20220531 16:42

--- hotel/src/main/java/com/care/hotel/mypage/MyPageController.java
+++ hotel/src/main/java/com/care/hotel/mypage/MyPageController.java
@@ -1,4 +1,4 @@
-package com.care.hotel;
+package com.care.hotel.mypage;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -10,23 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyPageController {
 private static final Logger logger = LoggerFactory.getLogger(MyPageController.class);
-
 	
 	@RequestMapping(value = "/myPage", method = RequestMethod.GET)
 	public String myPage(Locale locale, Model model) {
-		logger.info("Welcome myPage! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		logger.info("MyPageController - myPage");
 		return "myPage";
 	}
 	
