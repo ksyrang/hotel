e51c24cc6539ae813171edc994055677b5f0e3e8
SeunghoChoi1 <99311667+SeunghoChoi1@users.noreply.github.com>
2022-06-04 16:16:38 +0900

    회원가입 우편검색만 가능 csh

--- hotel/src/main/java/com/care/hotel/member/memberController.java
+++ hotel/src/main/java/com/care/hotel/member/memberController.java
@@ -1,5 +1,39 @@
 package com.care.hotel.member;
 
-public class memberController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.care.hotel.member.DTO.memberDTO;
+import com.care.hotel.member.service.memberSvcImpl;
+import com.care.hotel.member.DTO.memberExDTO;
+
+public class memberController {
+	@Autowired private memberSvcImpl memberService;
+	
+	@PostMapping(value = "isExistId", produces = "application/json; charset=UTF-8")
+	@ResponseBody
+	public String isExistId(@RequestBody(required = false) String id) {
+		String msg = memberService.isExistId(id);
+		return msg;
+	}
+	
+	@RequestMapping(value = "memberInsert")
+	public String memberInsert(memberDTO memberDTO, memberExDTO memberExDTO, Model model, RedirectAttributes ra) {
+		String msg = memberService.memberInsert(memberDTO, memberExDTO);
+	
+		if(msg.equals("가입 완료")) {
+			ra.addFlashAttribute("msg", msg);
+			return "redirect:/index?formpath=memberInsert";
+		}else {
+			model.addAttribute("msg", msg);
+			return "forward:/index?formpath=memJoinForm";
+		}
+		
+	}
+	
 }
