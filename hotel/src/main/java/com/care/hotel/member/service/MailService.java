7a84beec6719e9ce78f6b98dcf7ba0062387b465
SeunghoChoi1 <99311667+SeunghoChoi1@users.noreply.github.com>
2022-06-21 11:05:36 +0900

    csh 인증번호 메일 내용 변경

--- hotel/src/main/java/com/care/hotel/member/service/MailService.java
+++ hotel/src/main/java/com/care/hotel/member/service/MailService.java
@@ -16,10 +16,10 @@ public class MailService {
 		MimeMessage message = mailSender.createMimeMessage();
 
 		try {
-			
+			String content1 = "회원님의 인증번호는 " + content +" 입니다.";
 			MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
 			messageHelper.setSubject(subject); // 이메일의 제목
-			messageHelper.setText(content);// 이메일의 본문
+			messageHelper.setText(content1);// 이메일의 본문
 			messageHelper.setTo(to); // 수신자
 			
 			mailSender.send(message);
